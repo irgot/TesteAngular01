1e79f0497f8f61eb0b93a9791b79ab2698287923
Igor Souza <igor.ferreira.souza@gmail.com>
2019-09-03 11:16:12 -0300

    teste

--- teste01/src/app/app.module.ts
+++ teste01/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputPropertyComponent,
+    DataBindingComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule
---
+++ teste01/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  nomeDoCurso: string = 'Angular';
+  valorInicial: number= 15;
+  constructor() { }
+
+  onMudouValor(evento){
+    console.log(evento);
+  }
+  ngOnInit() {
+  }
+
+}
